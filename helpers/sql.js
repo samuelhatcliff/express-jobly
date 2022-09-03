8bcfbb9510a182943f50f310041ca27b6b4fbfe8
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-09-02 18:09:40 -0600

    added documentation for first task

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,14 +1,18 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+// This functions formats the data sent in the request body for patch requests for SQL queries
+// by converting it from js camel case to snake case
+// We return an object that contains appropriate format for each column with the corresponding parameterized query id
+// as well as the values pertaining to each column and parameterized query id
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  //Example: {"firstName": "first", "lastName": "last"} => [ 'first', 'last' ]
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  //Example: {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
